c5dd80f09618935f40faf050ab6b1fcd08340fd2
Katy Moe <katy@katy.moe>
2019-09-15 22:13:42 +0100

    edit cherry-picked merge commit messages

--- scripts/cherry_pick/cherry_pick.sh
+++ scripts/cherry_pick/cherry_pick.sh
@@ -10,7 +10,7 @@ echo "Cherry-picking changes..."
 git cherry-pick --no-commit --mainline 1 "$COMMIT_ID"
 
 echo "Unstaging files removed by us..."
-git status --short | sed -n 's/^DU //p' | ifne xargs git rm
+git status --short | gsed -n 's/^DU //p' | ifne xargs git rm
 
 echo "Unstaging files where SDK intentionally diverges from Core..."
 for f in $(git diff --name-only --cached | grep -Ef ./scripts/cherry_pick/IGNORE_FILES)
@@ -19,5 +19,10 @@ do
     git checkout -- "$f"
 done
 
+COMMIT_MSG=$(git log --format=%B -n 1 "$COMMIT_ID" | gsed -z -r 's/Merge pull request (#[0-9]+) from ([^\n]*\/[^\n]*)\n\n(.*$)/\3\nThis commit was generated from hashicorp\/terraform\1./g')
+
 echo "Committing changes. If this fails, you must resolve the merge conflict manually."
-git commit -C "$COMMIT_ID" && echo "Success!"
+git commit -C "$COMMIT_ID" && \
+# amend commit message afterwards to preserve authorship information
+git commit --amend --message "${COMMIT_MSG}" \
+&& echo "Success!"
