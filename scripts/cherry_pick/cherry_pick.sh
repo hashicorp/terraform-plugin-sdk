000a04a40df3a963471ed089036157284c399823
Katy Moe <katy@katy.moe>
2019-09-16 15:40:07 +0100

    please use GNU sed

--- scripts/cherry_pick/cherry_pick.sh
+++ scripts/cherry_pick/cherry_pick.sh
@@ -4,13 +4,19 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: ./scripts/cherry_pick/cherry_pick.sh MERGE_COMMIT_HASH"
 fi
 
+function pleaseUseGNUsed {
+    echo "Please install GNU sed to your PATH as 'sed'."
+    exit 1
+}
+sed --version > /dev/null || pleaseUseGNUsed
+
 COMMIT_ID=$1
 
 echo "Cherry-picking changes..."
 git cherry-pick --no-commit --mainline 1 "$COMMIT_ID"
 
 echo "Unstaging files removed by us..."
-git status --short | gsed -n 's/^DU //p' | ifne xargs git rm
+git status --short | sed -n 's/^DU //p' | ifne xargs git rm
 
 echo "Unstaging files where SDK intentionally diverges from Core..."
 for f in $(git diff --name-only --cached | grep -Ef ./scripts/cherry_pick/IGNORE_FILES)
@@ -19,7 +25,7 @@ do
     git checkout -- "$f"
 done
 
-COMMIT_MSG=$(git log --format=%B -n 1 "$COMMIT_ID" | gsed -z -r 's/Merge pull request (#[0-9]+) from ([^\n]*\/[^\n]*)\n\n(.*$)/\3\nThis commit was generated from hashicorp\/terraform\1./g')
+COMMIT_MSG=$(git log --format=%B -n 1 "$COMMIT_ID" | sed -z -r 's/Merge pull request (#[0-9]+) from ([^\n]*\/[^\n]*)\n\n(.*$)/\3\nThis commit was generated from hashicorp\/terraform\1./g')
 
 echo "Committing changes. If this fails, you must resolve the merge conflict manually."
 git commit -C "$COMMIT_ID" && \
