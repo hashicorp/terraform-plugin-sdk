ebd9d7144d587f7782d2b7ca4a14341b43d371f2
Alex Pilon <apilon@hashicorp.com>
2019-09-30 15:50:12 -0400

    create helper for acctests POC

--- helper/acctest/acctest.go
+++ helper/acctest/acctest.go
@@ -1,2 +1,22 @@
 // Package acctest contains for Terraform Acceptance Tests
 package acctest
+
+import (
+	"os"
+
+	tftest "github.com/apparentlymart/terraform-plugin-test"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+)
+
+func InitProviderTesting(name string, providerFunc plugin.ProviderFunc) *tftest.Helper {
+	if tftest.RunningAsPlugin() {
+		// The test program is being re-launched as a provider plugin via our
+		// stub program.
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: providerFunc,
+		})
+		os.Exit(0)
+	}
+
+	return tftest.AutoInitProviderHelper(name)
+}
