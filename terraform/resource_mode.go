08120f9b35b94a7a44f9b2ba319ab46734f7d8c8
Alex Pilon <apilon@hashicorp.com>
2019-11-19 13:42:17 -0500

    go mod tidy

--- terraform/resource_mode.go
+++ terraform/resource_mode.go
@@ -1,12 +1,12 @@
 package terraform
 
-//go:generate go run golang.org/x/tools/cmd/stringer -type=resourceMode -output=resource_mode_string.go resource_mode.go
+//go:generate go run golang.org/x/tools/cmd/stringer -type=ResourceMode -output=resource_mode_string.go resource_mode.go
 
-// resourceMode is deprecated, use addrs.ResourceMode instead.
+// ResourceMode is deprecated, use addrs.ResourceMode instead.
 // It has been preserved for backwards compatibility.
-type resourceMode int
+type ResourceMode int
 
 const (
-	managedResourceMode resourceMode = iota
-	dataResourceMode
+	ManagedResourceMode ResourceMode = iota
+	DataResourceMode
 )
