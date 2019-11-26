8f060c568e0f747696f5a0e8701c6d3bfd9cd8f8
Katy Moe <katy@katy.moe>
2019-11-26 18:51:15 +0000

    ensure state is refreshed before id-only refresh test

--- helper/resource/testing_new.go
+++ helper/resource/testing_new.go
@@ -329,14 +329,8 @@ func testStepNewImportState(t *testing.T, c TestCase, wd *tftest.WorkingDir, ste
 		}
 	}
 
-	// TODO: this was copy pasted from the old framework
-	// perhaps it can be cleaned up
 	// Verify that all the states match
 	if step.ImportStateVerify {
-		// attach providers for ImportStateVerify
-		// step.providers = providers
-		t.Logf("step.providers: %+v", step.providers)
-
 		new := importState.RootModule().Resources
 		old := state.RootModule().Resources
 
@@ -487,40 +481,7 @@ func testStepNewConfig(t *testing.T, c TestCase, wd *tftest.WorkingDir, step Tes
 		}
 	}
 
-	// ID-ONLY REFRESH
-	// If we've never checked an id-only refresh and our state isn't
-	// empty, find the first resource and test it.
-	state := getState(t, wd)
-	if idRefresh && idRefreshCheck == nil && !state.Empty() {
-		// Find the first non-nil resource in the state
-		for _, m := range state.Modules {
-			if len(m.Resources) > 0 {
-				if v, ok := m.Resources[c.IDRefreshName]; ok {
-					idRefreshCheck = v
-				}
-
-				break
-			}
-		}
-
-		// If we have an instance to check for refreshes, do it
-		// immediately. We do it in the middle of another test
-		// because it shouldn't affect the overall state (refresh
-		// is read-only semantically) and we want to fail early if
-		// this fails. If refresh isn't read-only, then this will have
-		// caught a different bug.
-		if idRefreshCheck != nil {
-			t.Logf(
-				"[WARN] Test: Running ID-only refresh check on %s",
-				idRefreshCheck.Primary.ID)
-			if err := testIDRefresh(c, t, wd, step, idRefreshCheck); err != nil {
-				t.Fatalf(
-					"[ERROR] Test: ID-only test failed: %s", err)
-			}
-		}
-	}
-
-	// DESTROY / CLEANUP
+	// Test for perpetual diffs by performing a plan, a refresh, and another plan
 
 	// do a plan
 	wd.RequireCreatePlan(t)
@@ -553,6 +514,36 @@ func testStepNewConfig(t *testing.T, c TestCase, wd *tftest.WorkingDir, step Tes
 		} else {
 
 			t.Fatalf("After applying this step, the plan was not empty. %s", spewConf.Sdump(plan)) // TODO error message
+		}
+	}
+
+	// ID-ONLY REFRESH
+	// If we've never checked an id-only refresh and our state isn't
+	// empty, find the first resource and test it.
+	state := getState(t, wd)
+	if idRefresh && idRefreshCheck == nil && !state.Empty() {
+		// Find the first non-nil resource in the state
+		for _, m := range state.Modules {
+			if len(m.Resources) > 0 {
+				if v, ok := m.Resources[c.IDRefreshName]; ok {
+					idRefreshCheck = v
+				}
+
+				break
+			}
+		}
+
+		// If we have an instance to check for refreshes, do it
+		// immediately. We do it in the middle of another test
+		// because it shouldn't affect the overall state (refresh
+		// is read-only semantically) and we want to fail early if
+		// this fails. If refresh isn't read-only, then this will have
+		// caught a different bug.
+		if idRefreshCheck != nil {
+			if err := testIDRefresh(c, t, wd, step, idRefreshCheck); err != nil {
+				t.Fatalf(
+					"[ERROR] Test: ID-only test failed: %s", err)
+			}
 		}
 	}
 
