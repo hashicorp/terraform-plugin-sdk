4b04df2587486b22728ae7e8eb4b9b03e08a620f
Alex Pilon <apilon@hashicorp.com>
2020-02-19 17:47:49 -0500

    prune internal/addrs

--- internal/addrs/instance_key.go
+++ internal/addrs/instance_key.go
@@ -2,9 +2,6 @@ package addrs
 
 import (
 	"fmt"
-
-	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/gocty"
 )
 
 // instanceKey represents the key of an instance within an object that
@@ -18,26 +15,6 @@ import (
 type instanceKey interface {
 	instanceKeySigil()
 	String() string
-}
-
-// parseInstanceKey returns the instance key corresponding to the given value,
-// which must be known and non-null.
-//
-// If an unknown or null value is provided then this function will panic. This
-// function is intended to deal with the values that would naturally be found
-// in a hcl.TraverseIndex, which (when parsed from source, at least) can never
-// contain unknown or null values.
-func parseInstanceKey(key cty.Value) (instanceKey, error) {
-	switch key.Type() {
-	case cty.String:
-		return stringKey(key.AsString()), nil
-	case cty.Number:
-		var idx int
-		err := gocty.FromCtyValue(key, &idx)
-		return intKey(idx), err
-	default:
-		return NoKey, fmt.Errorf("either a string or an integer is required")
-	}
 }
 
 // NoKey represents the absense of an instanceKey, for the single instance
