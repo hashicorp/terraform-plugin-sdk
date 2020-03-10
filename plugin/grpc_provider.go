0a3c887a457ccf545dab9e96d3a91f01ef3eadff
Alex Pilon <apilon@hashicorp.com>
2020-03-10 15:05:28 -0400

    fix optional interface implentation bug ensure struct implements the two interfaces of go-plugin remove embedded interface and explicitly implement methods return errors for all methods other than grpc server method.

--- plugin/grpc_provider.go
+++ plugin/grpc_provider.go
@@ -2,6 +2,8 @@ package plugin
 
 import (
 	"context"
+	"errors"
+	"net/rpc"
 
 	plugin "github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
@@ -9,16 +11,28 @@ import (
 	proto "github.com/hashicorp/terraform-plugin-sdk/v2/internal/tfplugin5"
 )
 
-// GRPCProviderPlugin implements plugin.GRPCPlugin for the go-plugin package.
+var (
+	_ plugin.GRPCPlugin = (*gRPCProviderPlugin)(nil)
+	_ plugin.Plugin     = (*gRPCProviderPlugin)(nil)
+)
+
+// gRPCProviderPlugin implements plugin.GRPCPlugin and plugin.Plugin for the go-plugin package.
+// the only real implementation is GRPCSServer, the other methods are only satisfied
+// for compatibility with go-plugin
 type gRPCProviderPlugin struct {
-	plugin.Plugin
 	GRPCProvider func() proto.ProviderServer
 }
 
-// this exists only to satisfy the go-plugin.GRPCPlugin interface
-// that interface should likely be split
-func (p *gRPCProviderPlugin) GRPCClient(ctx context.Context, broker *plugin.GRPCBroker, c *grpc.ClientConn) (interface{}, error) {
-	return nil, nil
+func (p *gRPCProviderPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
+	return nil, errors.New("terraform-plugin-sdk only implements grpc servers")
+}
+
+func (p *gRPCProviderPlugin) Client(*plugin.MuxBroker, *rpc.Client) (interface{}, error) {
+	return nil, errors.New("terraform-plugin-sdk only implements grpc servers")
+}
+
+func (p *gRPCProviderPlugin) GRPCClient(context.Context, *plugin.GRPCBroker, *grpc.ClientConn) (interface{}, error) {
+	return nil, errors.New("terraform-plugin-sdk only implements grpc servers")
 }
 
 func (p *gRPCProviderPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
