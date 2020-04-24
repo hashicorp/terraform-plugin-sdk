008f6de9336ef2ec4a1e0bada0f83aaccf476e25
Brian Flad <bflad417@gmail.com>
2020-04-23 20:37:31 -0400

    helper/schema: Add missing argument from rebase

--- helper/schema/schema.go
+++ helper/schema/schema.go
@@ -757,7 +757,7 @@ func (m schemaMap) internalValidate(topSchemaMap schemaMap, attrsOnly bool) erro
 		}
 
 		if len(v.RequiredWith) > 0 {
-			err := checkKeysAgainstSchemaFlags(k, v.RequiredWith, topSchemaMap)
+			err := checkKeysAgainstSchemaFlags(k, v.RequiredWith, topSchemaMap, v)
 			if err != nil {
 				return fmt.Errorf("RequiredWith: %+v", err)
 			}
