c93a50f0291d234b1f333783ef178c647625f86e
Andreas Bergmeier <abergmeier@gmx.net>
2020-09-26 12:55:32 +0200

    Fix warnings around errors
    Use correct function and remove ending exclamation mark.

--- helper/resource/testing_new_config.go
+++ helper/resource/testing_new_config.go
@@ -1,6 +1,7 @@
 package resource
 
 import (
+	"errors"
 	"fmt"
 
 	tfjson "github.com/hashicorp/terraform-json"
@@ -186,7 +187,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *tftest.WorkingDir, step Test
 		}
 		return fmt.Errorf("After applying this test step and performing a `terraform refresh`, the plan was not empty.\nstdout\n\n%s", stdout)
 	} else if step.ExpectNonEmptyPlan && planIsEmpty(plan) {
-		return fmt.Errorf("Expected a non-empty plan, but got an empty plan!")
+		return errors.New("Expected a non-empty plan, but got an empty plan")
 	}
 
 	// ID-ONLY REFRESH
