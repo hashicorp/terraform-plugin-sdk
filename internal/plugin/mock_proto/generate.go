7720697ffc4e238dc449f0918ba5d6edb562c7b8
Katy Moe <katy@katy.moe>
2020-10-09 16:21:49 +0100

    remove tfplugin5 package
    The tfplugin5 package is no longer used; terraform-plugin-go is now the common interface for the Terraform protocol.

--- internal/plugin/mock_proto/generate.go
+++
@@ -1,3 +0,0 @@
-//go:generate go run github.com/golang/mock/mockgen -destination mock.go github.com/hashicorp/terraform-plugin-sdk/v2/internal/tfplugin5 ProviderClient,ProvisionerClient,Provisioner_ProvisionResourceClient,Provisioner_ProvisionResourceServer
-
-package mock_tfplugin5
