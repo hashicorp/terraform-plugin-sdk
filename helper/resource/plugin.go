e82e59e4a1788ad9e99c19eafe0e8ecf55565834
Katy Moe <katy@katy.moe>
2021-05-07 15:03:41 +0100

    add ProtoV6ProviderFactories to TestCase
    In order to support protocol v6 providers in the test suite, we add another
    field to TestCase along the same lines as ProtoV5ProviderFactories. This is
    plumbed through as yet another argument in runProviderCommand. We hope that by
    the time another protocol major version is released, a new, more flexible test
    framework will be available.
    
    plugin.ServeOpts and plugin.DebugServe also expected all provider servers to be
    v5 servers, so we also add the GRPCProviderV6Func field to ServeOpts to keep
    it backwards compatible. If both GRPCProviderFunc and GRPCProviderV6Func are
    set, the v6 server will be ignored.

--- helper/resource/plugin.go
+++ helper/resource/plugin.go
@@ -12,13 +12,14 @@ import (
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/internal/plugintest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	testing "github.com/mitchellh/go-testing-interface"
 )
 
-func runProviderCommand(t testing.T, f func() error, wd *plugintest.WorkingDir, factories map[string]func() (*schema.Provider, error), v5factories map[string]func() (tfprotov5.ProviderServer, error)) error {
+func runProviderCommand(t testing.T, f func() error, wd *plugintest.WorkingDir, factories map[string]func() (*schema.Provider, error), v5factories map[string]func() (tfprotov5.ProviderServer, error), v6factories map[string]func() (tfprotov6.ProviderServer, error)) error {
 	// don't point to this as a test failure location
 	// point to whatever called it
 	t.Helper()
@@ -94,9 +95,10 @@ func runProviderCommand(t testing.T, f func() error, wd *plugintest.WorkingDir, 
 			return fmt.Errorf("unable to serve provider %q: %w", providerName, err)
 		}
 		tfexecConfig := tfexec.ReattachConfig{
-			Protocol: config.Protocol,
-			Pid:      config.Pid,
-			Test:     config.Test,
+			Protocol:        config.Protocol,
+			ProtocolVersion: config.ProtocolVersion,
+			Pid:             config.Pid,
+			Test:            config.Test,
 			Addr: tfexec.ReattachConfigAddr{
 				Network: config.Addr.Network,
 				String:  config.Addr.String,
@@ -120,7 +122,7 @@ func runProviderCommand(t testing.T, f func() error, wd *plugintest.WorkingDir, 
 		}
 	}
 
-	// Now spin up gRPC servers for every plugin-go provider factory
+	// Now spin up gRPC servers for every protov5 provider factory
 	// in the same way.
 	for providerName, factory := range v5factories {
 		// providerName may be returned as terraform-provider-foo, and
@@ -170,9 +172,85 @@ func runProviderCommand(t testing.T, f func() error, wd *plugintest.WorkingDir, 
 			return fmt.Errorf("unable to serve provider %q: %w", providerName, err)
 		}
 		tfexecConfig := tfexec.ReattachConfig{
-			Protocol: config.Protocol,
-			Pid:      config.Pid,
-			Test:     config.Test,
+			Protocol:        config.Protocol,
+			ProtocolVersion: config.ProtocolVersion,
+			Pid:             config.Pid,
+			Test:            config.Test,
+			Addr: tfexec.ReattachConfigAddr{
+				Network: config.Addr.Network,
+				String:  config.Addr.String,
+			},
+		}
+
+		// when the provider exits, remove one from the waitgroup
+		// so we can track when everything is done
+		go func(c <-chan struct{}) {
+			<-c
+			wg.Done()
+		}(closeCh)
+
+		// set our provider's reattachinfo in our map, once
+		// for every namespace that different Terraform versions
+		// may expect.
+		for _, ns := range namespaces {
+			reattachString := strings.TrimSuffix(host, "/") + "/" +
+				strings.TrimSuffix(ns, "/") + "/" +
+				providerName
+			reattachInfo[reattachString] = tfexecConfig
+		}
+	}
+
+	// Now spin up gRPC servers for every protov6 provider factory
+	// in the same way.
+	for providerName, factory := range v6factories {
+		// providerName may be returned as terraform-provider-foo, and
+		// we need just foo. So let's fix that.
+		providerName = strings.TrimPrefix(providerName, "terraform-provider-")
+
+		// If the user has already registered this provider in
+		// ProviderFactories or ProtoV5ProviderFactories, they made a
+		// mistake and we should exit early.
+		for _, ns := range namespaces {
+			reattachString := strings.TrimSuffix(host, "/") + "/" +
+				strings.TrimSuffix(ns, "/") + "/" +
+				providerName
+			if _, ok := reattachInfo[reattachString]; ok {
+				return fmt.Errorf("Provider %s registered in both TestCase.ProtoV6ProviderFactories and either TestCase.ProviderFactories or TestCase.ProtoV5ProviderFactories: please use one of the three, or supply a muxed provider to TestCase.ProtoV5ProviderFactories.", providerName)
+			}
+		}
+
+		provider, err := factory()
+		if err != nil {
+			return fmt.Errorf("unable to create provider %q from factory: %w", providerName, err)
+		}
+
+		// keep track of the running factory, so we can make sure it's
+		// shut down.
+		wg.Add(1)
+
+		opts := &plugin.ServeOpts{
+			GRPCProviderV6Func: func() tfprotov6.ProviderServer {
+				return provider
+			},
+			Logger: hclog.New(&hclog.LoggerOptions{
+				Name:   "plugintest",
+				Level:  hclog.Trace,
+				Output: ioutil.Discard,
+			}),
+			NoLogOutputOverride: true,
+		}
+
+		// let's actually start the provider server
+		config, closeCh, err := plugin.DebugServe(ctx, opts)
+		if err != nil {
+			return fmt.Errorf("unable to serve provider %q: %w", providerName, err)
+		}
+
+		tfexecConfig := tfexec.ReattachConfig{
+			Protocol:        config.Protocol,
+			ProtocolVersion: config.ProtocolVersion,
+			Pid:             config.Pid,
+			Test:            config.Test,
 			Addr: tfexec.ReattachConfigAddr{
 				Network: config.Addr.Network,
 				String:  config.Addr.String,
--- helper/resource/testing_new_config.go
+++ helper/resource/testing_new_config.go
@@ -26,7 +26,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 				return err
 			}
 			return nil
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return err
 		}
@@ -44,7 +44,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 	// failing to do this will result in data sources not being updated
 	err = runProviderCommand(t, func() error {
 		return wd.Refresh()
-	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 	if err != nil {
 		return fmt.Errorf("Error running pre-apply refresh: %w", err)
 	}
@@ -59,7 +59,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 				return wd.CreateDestroyPlan()
 			}
 			return wd.CreatePlan()
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return fmt.Errorf("Error running pre-apply plan: %w", err)
 		}
@@ -74,7 +74,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 				return err
 			}
 			return nil
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return fmt.Errorf("Error retrieving pre-apply state: %w", err)
 		}
@@ -82,7 +82,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 		// Apply the diff, creating real resources
 		err = runProviderCommand(t, func() error {
 			return wd.Apply()
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			if step.Destroy {
 				return fmt.Errorf("Error running destroy: %w", err)
@@ -98,7 +98,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 				return err
 			}
 			return nil
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return fmt.Errorf("Error retrieving state after apply: %w", err)
 		}
@@ -126,7 +126,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 			return wd.CreateDestroyPlan()
 		}
 		return wd.CreatePlan()
-	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 	if err != nil {
 		return fmt.Errorf("Error running post-apply plan: %w", err)
 	}
@@ -136,7 +136,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 		var err error
 		plan, err = wd.SavedPlan()
 		return err
-	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 	if err != nil {
 		return fmt.Errorf("Error retrieving post-apply plan: %w", err)
 	}
@@ -147,7 +147,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 			var err error
 			stdout, err = wd.SavedPlanRawStdout()
 			return err
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return fmt.Errorf("Error retrieving formatted plan output: %w", err)
 		}
@@ -158,7 +158,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 	if !step.Destroy || (step.Destroy && !step.PreventPostDestroyRefresh) {
 		err := runProviderCommand(t, func() error {
 			return wd.Refresh()
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return fmt.Errorf("Error running post-apply refresh: %w", err)
 		}
@@ -170,7 +170,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 			return wd.CreateDestroyPlan()
 		}
 		return wd.CreatePlan()
-	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 	if err != nil {
 		return fmt.Errorf("Error running second post-apply plan: %w", err)
 	}
@@ -179,7 +179,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 		var err error
 		plan, err = wd.SavedPlan()
 		return err
-	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 	if err != nil {
 		return fmt.Errorf("Error retrieving second post-apply plan: %w", err)
 	}
@@ -191,7 +191,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 			var err error
 			stdout, err = wd.SavedPlanRawStdout()
 			return err
-		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+		}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 		if err != nil {
 			return fmt.Errorf("Error retrieving formatted second plan output: %w", err)
 		}
@@ -210,7 +210,7 @@ func testStepNewConfig(t testing.T, c TestCase, wd *plugintest.WorkingDir, step 
 			return err
 		}
 		return nil
-	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories)
+	}, wd, c.ProviderFactories, c.ProtoV5ProviderFactories, c.ProtoV6ProviderFactories)
 	if err != nil {
 		return err
 	}
