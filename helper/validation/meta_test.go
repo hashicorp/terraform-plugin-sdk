a0d2e21239c1dad1134febf36a108692e663fa81
Brian Flad <bflad417@gmail.com>
2022-03-28 10:06:15 -0400

    helper/validation: Prevented panics with `ToDiagFunc()` function when used inside `Schema` type `Elem` field (#915)
    Reference: https://github.com/hashicorp/terraform-plugin-sdk/issues/734

--- helper/validation/meta_test.go
+++ helper/validation/meta_test.go
@@ -3,6 +3,9 @@ package validation
 import (
 	"regexp"
 	"testing"
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func TestValidationNoZeroValues(t *testing.T) {
@@ -101,36 +104,94 @@ func TestValidationAny(t *testing.T) {
 }
 
 func TestToDiagFunc(t *testing.T) {
-	runDiagTestCases(t, []diagTestCase{
-		{
+	t.Parallel()
+
+	testCases := map[string]struct {
+		path        cty.Path
+		val         interface{}
+		f           schema.SchemaValidateDiagFunc
+		expectedErr *regexp.Regexp
+	}{
+		"success-GetAttrStep-int": {
+			path: cty.Path{
+				cty.GetAttrStep{Name: "test_property"},
+			},
 			val: 43,
 			f: ToDiagFunc(Any(
 				IntAtLeast(42),
 				IntAtMost(5),
 			)),
 		},
-		{
+		"success-GetAttrStep-string": {
+			path: cty.Path{
+				cty.GetAttrStep{Name: "test_property"},
+			},
 			val: "foo",
 			f: ToDiagFunc(All(
 				StringLenBetween(1, 10),
 				StringIsNotWhiteSpace,
 			)),
 		},
-		{
+		"success-IndexStep-string": {
+			path: cty.Path{
+				cty.GetAttrStep{Name: "test_property"},
+				cty.IndexStep{Key: cty.NumberIntVal(0)},
+			},
+			val: "foo",
+			f:   ToDiagFunc(StringLenBetween(1, 10)),
+		},
+		"error-GetAttrStep-int-first": {
+			path: cty.Path{
+				cty.GetAttrStep{Name: "test_property"},
+			},
 			val: 7,
 			f: ToDiagFunc(Any(
 				IntAtLeast(42),
 				IntAtMost(5),
 			)),
-			expectedErr: regexp.MustCompile(`expected [\w]+ to be at least \(42\), got 7`),
+			expectedErr: regexp.MustCompile(`expected test_property to be at least \(42\), got 7`),
 		},
-		{
+		"error-GetAttrStep-int-second": {
+			path: cty.Path{
+				cty.GetAttrStep{Name: "test_property"},
+			},
 			val: 7,
 			f: ToDiagFunc(Any(
 				IntAtLeast(42),
 				IntAtMost(5),
 			)),
-			expectedErr: regexp.MustCompile(`expected [\w]+ to be at most \(5\), got 7`),
+			expectedErr: regexp.MustCompile(`expected test_property to be at most \(5\), got 7`),
 		},
-	})
+		"error-IndexStep-int": {
+			path: cty.Path{
+				cty.GetAttrStep{Name: "test_property"},
+				cty.IndexStep{Key: cty.NumberIntVal(0)},
+			},
+			val:         7,
+			f:           ToDiagFunc(IntAtLeast(42)),
+			expectedErr: regexp.MustCompile(`expected test_property to be at least \(42\), got 7`),
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			diags := testCase.f(testCase.val, testCase.path)
+
+			if !diags.HasError() && testCase.expectedErr == nil {
+				return
+			}
+
+			if diags.HasError() && testCase.expectedErr == nil {
+				t.Fatalf("expected to produce no errors, got %v", diags)
+			}
+
+			if !matchAnyDiagSummary(diags, testCase.expectedErr) {
+				t.Fatalf("expected to produce error matching %q, got %v", testCase.expectedErr, diags)
+			}
+		})
+	}
 }
