7bc4c6ed4b582b7f3f9f64d1a47129f7463b1cf2
Brian Flad <bflad417@gmail.com>
2022-11-07 15:32:39 -0500

    helper/resource: Various small fixes (#1091)
    Reference: https://github.com/hashicorp/terraform-plugin-sdk/issues/1063
    Reference: https://github.com/hashicorp/go-plugin/pull/220
    
    This includes the following small bug fixes:
    
    - Clarifies/fixes the trace logging during `terraform show` commands
    - Improves performance slightly for `terraform show` raw plan usage (terraform-exec already captures and returns stdout for raw plans)
    - Prevents a goroutine leak specific to the detached stop context in `schema.GRPCProvider`
    
    Previously, leak detection on testing would contain an entry per Terraform command such as:
    
    ```
            [Goroutine 276 in state select, with github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema.mergeStop on top of the stack:
            goroutine 276 [select]:
            github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema.mergeStop({0x103a5f298?, 0x14000390c00?}, 0x1400033e5f0, 0x140004bb560)
                    /Users/bflad/src/github.com/hashicorp/terraform-plugin-sdk/helper/schema/grpc_provider.go:46 +0x64
            created by github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema.(*GRPCProviderServer).StopContext
                    /Users/bflad/src/github.com/hashicorp/terraform-plugin-sdk/helper/schema/grpc_provider.go:63 +0x178
            ]
    ```
    
    Leaked goroutines such as:
    
    ```
            [Goroutine 53 in state select, with github.com/hashicorp/go-plugin.(*gRPCBrokerServer).Recv on top of the stack:
            goroutine 53 [select]:
            github.com/hashicorp/go-plugin.(*gRPCBrokerServer).Recv(0x29?)
                    /Users/bflad/go/pkg/mod/github.com/hashicorp/go-plugin@v1.4.4/grpc_broker.go:121 +0x58
            github.com/hashicorp/go-plugin.(*GRPCBroker).Run(0x140003ca190)
                    /Users/bflad/go/pkg/mod/github.com/hashicorp/go-plugin@v1.4.4/grpc_broker.go:411 +0x40
            created by github.com/hashicorp/go-plugin.(*GRPCServer).Init
                    /Users/bflad/go/pkg/mod/github.com/hashicorp/go-plugin@v1.4.4/grpc_server.go:85 +0x424
            ]
    ```
    
    Will likely require a fix upstream in go-plugin.

--- helper/resource/plugin_test.go
+++ helper/resource/plugin_test.go
@@ -1,13 +1,17 @@
 package resource
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/internal/plugintest"
 )
 
 func TestProtoV5ProviderFactoriesMerge(t *testing.T) {
@@ -232,5 +236,64 @@ func TestSdkProviderFactoriesMerge(t *testing.T) {
 				t.Errorf("unexpected difference: %s", diff)
 			}
 		})
+	}
+}
+
+func TestRunProviderCommand(t *testing.T) {
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		t.Fatalf("unable to get working directory: %s", err)
+	}
+
+	ctx := context.Background()
+	funcCalled := false
+	helper := plugintest.AutoInitProviderHelper(ctx, currentDir)
+
+	err = runProviderCommand(
+		ctx,
+		t,
+		func() error {
+			funcCalled = true
+			return nil
+		},
+		helper.RequireNewWorkingDir(ctx, t),
+		&providerFactories{
+			legacy: map[string]func() (*schema.Provider, error){
+				"examplecloud": func() (*schema.Provider, error) { //nolint:unparam // required signature
+					return &schema.Provider{
+						ResourcesMap: map[string]*schema.Resource{
+							"examplecloud_thing": {
+								CreateContext: func(ctx context.Context, d *schema.ResourceData, i interface{}) diag.Diagnostics {
+									d.SetId("id")
+
+									return nil
+								},
+								DeleteContext: func(_ context.Context, _ *schema.ResourceData, _ interface{}) diag.Diagnostics {
+									return nil
+								},
+								ReadContext: func(_ context.Context, d *schema.ResourceData, _ interface{}) diag.Diagnostics {
+									return nil
+								},
+								Schema: map[string]*schema.Schema{
+									"id": {
+										Computed: true,
+										Type:     schema.TypeString,
+									},
+								},
+							},
+						},
+					}, nil
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !funcCalled {
+		t.Error("expected func to be called")
 	}
 }
