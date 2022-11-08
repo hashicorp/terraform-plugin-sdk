4d089448be92021f7c1bfbf9c5af5191d1a50b06
Brian Flad <bflad417@gmail.com>
2022-11-08 15:13:34 -0500

    More testing

--- helper/resource/plugin_test.go
+++ helper/resource/plugin_test.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/go-hclog"
+	goplugin "github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
@@ -18,7 +21,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
-func testRunProviderCommand(t *testing.T, ctx context.Context, opts *plugin.ServeOpts, wd *plugintest.WorkingDir, f func() error) {
+/*
+j terraform-plugin-sdk
+TF_LOG=TRACE go test -count=100 -parallel=1 -run='TestTest_TestStep_ProviderFactories_Import_Inline_WithPersistMatch' -v ./helper/resource
+TF_ACC_LOG=TRACE TF_ACC_LOG_PATH=$(pwd)/trace.txt TF_LOG_SDK_PROTO=OFF TF_LOG_SDK_HELPER_SCHEMA=OFF TF_LOG_SDK_HELPER_RESOURCE=OFF go test -count=100 -parallel=1 -run='TestTest_TestStep_ProviderFactories_Import_Inline_WithPersistMatch' -v ./helper/resource
+go test -count=1 -parallel=1 -run='TestPanicAtTheDisco' -v ./helper/resource
+*/
+
+// func testRunProviderCommand(t *testing.T, ctx context.Context, opts *plugin.ServeOpts, wd *plugintest.WorkingDir, f func() error) {
+func testRunProviderCommand(t *testing.T, ctx context.Context, opts *plugin.ServeOpts, tf *tfexec.Terraform, f func(reattachInfo tfexec.ReattachInfo) error) {
 	t.Helper()
 
 	ctx, cancel := context.WithCancel(ctx)
@@ -32,50 +43,144 @@ func testRunProviderCommand(t *testing.T, ctx context.Context, opts *plugin.Serv
 		t.Fatalf("unable to serve provider: %s", err)
 	}
 
-	tfexecConfig := tfexec.ReattachConfig{
-		Protocol:        config.Protocol,
-		ProtocolVersion: config.ProtocolVersion,
-		Pid:             config.Pid,
-		Test:            config.Test,
-		Addr: tfexec.ReattachConfigAddr{
-			Network: config.Addr.Network,
-			String:  config.Addr.String,
+	// tfexecConfig := tfexec.ReattachConfig{
+	// 	Protocol:        config.Protocol,
+	// 	ProtocolVersion: config.ProtocolVersion,
+	// 	Pid:             config.Pid,
+	// 	Test:            config.Test,
+	// 	Addr: tfexec.ReattachConfigAddr{
+	// 		Network: config.Addr.Network,
+	// 		String:  config.Addr.String,
+	// 	},
+	// }
+
+	// reattachInfo := map[string]tfexec.ReattachConfig{
+	// 	opts.ProviderAddr: tfexecConfig,
+	// }
+
+	//wd.SetReattachInfo(ctx, reattachInfo)
+
+	// t.Log("running command")
+
+	// if err := f(reattachInfo); err != nil {
+	// 	t.Errorf("error running Terraform: %s", err)
+	// }
+
+	//wd.UnsetReattachInfo()
+
+	client := goplugin.NewClient(&goplugin.ClientConfig{
+		HandshakeConfig: plugin.Handshake,
+		Plugins: goplugin.PluginSet{
+			"provider": nil,
 		},
+		Reattach: &goplugin.ReattachConfig{
+			Protocol:        goplugin.Protocol(config.Protocol),
+			ProtocolVersion: config.ProtocolVersion,
+			Pid:             config.Pid,
+			Test:            config.Test,
+			Addr: &net.UnixAddr{
+				Name: config.Addr.String,
+				Net:  "unix",
+			},
+		},
+	})
+
+	t.Log("creating client")
+
+	_, err = client.Client()
+
+	if err != nil {
+		t.Fatalf("error creating client: %s", err)
 	}
 
-	reattachInfo := map[string]tfexec.ReattachConfig{
-		opts.ProviderAddr: tfexecConfig,
-	}
+	defer client.Kill()
 
-	wd.SetReattachInfo(ctx, reattachInfo)
+	// providerClientIface, err := rpcClient.Dispense("provider")
 
-	t.Log("running command")
+	// if err != nil {
+	// 	t.Fatalf("error dispensing RPC client: %s", err)
+	// }
 
-	if err := f(); err != nil {
-		t.Errorf("error running Terraform: %s", err)
-	}
+	// providerClient, ok := providerClientIface.(...)
 
-	wd.UnsetReattachInfo()
+	// if !ok {
+	// 	t.Fatalf("expected something, got: %T", providerClientIface)
+	// }
+
+	// providerClient.StopProvider(ctx, nil)
 }
 
 func TestPanicAtTheDisco(t *testing.T) {
 	t.Setenv("CHECKPOINT_DISABLE", "1")
 
-	currentDir, err := os.Getwd()
+	// currentDir, err := os.Getwd()
 
-	if err != nil {
-		t.Fatalf("unable to get working directory: %s", err)
-	}
+	// if err != nil {
+	// 	t.Fatalf("unable to get working directory: %s", err)
+	// }
 
 	ctx := context.Background()
 
-	helper := plugintest.AutoInitProviderHelper(ctx, currentDir)
-	wd := helper.RequireNewWorkingDir(ctx, t)
-	t.Logf("working directory: %s", wd.GetHelper().WorkingDirectory())
-
-	if err := wd.SetConfig(ctx, `resource "test_thing" "test" {}`); err != nil {
-		t.Fatalf("error setting configuration: %s", err)
+	//helper := plugintest.AutoInitProviderHelper(ctx, currentDir)
+	baseDir, err := os.MkdirTemp("", "plugintest")
+	if err != nil {
+		t.Fatalf("failed to create temporary directory for test helper: %s", err)
 	}
+	workDir, err := os.MkdirTemp(baseDir, "work")
+	if err != nil {
+		t.Fatalf("failed to create work dir: %s", err)
+	}
+	// tfDir, err := os.MkdirTemp("", "plugintest-terraform")
+	// if err != nil {
+	// 	t.Fatalf("failed to create temp dir: %s", err)
+	// }
+	// &Config{
+	// 	SourceDir:     currentDir,
+	// 	TerraformExec: "/opt/homebrew/bin/terraform",
+	// 	execTempDir:   tfDir,
+	// }
+
+	// &WorkingDir{
+	// 	h: &Helper{
+	// 		baseDir:       baseDir,
+	// 		sourceDir:     currentDir,
+	// 		terraformExec: "/opt/homebrew/bin/terraform",
+	// 		execTempDir:   tfDir,
+	// 	},
+	// 	tf:            tf,
+	// 	baseDir:       dir,
+	// 	terraformExec: "/opt/homebrew/bin/terraform",
+	// }
+	tf, err := tfexec.NewTerraform(workDir, "/opt/homebrew/bin/terraform")
+
+	if err != nil {
+		t.Fatalf("unable to create terraform-exec instance: %s", err)
+	}
+
+	if err = tf.SetDisablePluginTLS(true); err != nil {
+		t.Fatalf("unable to disable terraform-exec plugin TLS: %s", err)
+	}
+
+	// chili pepper
+	// zucchini
+	// soybeans
+
+	if err := tf.SetLog("TRACE"); err != nil {
+		t.Fatalf("unable to set log to TRACE: %s", err)
+	}
+
+	//tf.SetStderr(os.Stderr)
+
+	//wd := helper.RequireNewWorkingDir(ctx, t)
+	t.Logf("working directory: %s", baseDir)
+
+	if err := os.WriteFile(filepath.Join(workDir, "main.tf"), []byte(`resource "test_thing" "test" {}`), 0700); err != nil {
+		t.Fatalf("unable to write configuration file: %s", err)
+	}
+
+	// if err := wd.SetConfig(ctx); err != nil {
+	// 	t.Fatalf("error setting configuration: %s", err)
+	// }
 
 	provider := &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
@@ -121,30 +226,73 @@ func TestPanicAtTheDisco(t *testing.T) {
 	}
 
 	t.Log("Calling init")
-	testRunProviderCommand(t, ctx, opts, wd, func() error {
-		return wd.Init(ctx)
+	testRunProviderCommand(t, ctx, opts, tf, func(reattachInfo tfexec.ReattachInfo) error {
+		return tf.Init(ctx, tfexec.Reattach(reattachInfo), tfexec.Upgrade(true))
 	})
 
 	t.Log("Calling plan")
-	testRunProviderCommand(t, ctx, opts, wd, func() error {
-		return wd.CreatePlan(ctx)
+	testRunProviderCommand(t, ctx, opts, tf, func(reattachInfo tfexec.ReattachInfo) error {
+		hasChanges, err := tf.Plan(ctx, tfexec.Reattach(reattachInfo), tfexec.Refresh(false), tfexec.Out("tfplan"))
+
+		if err != nil {
+			return err
+		}
+
+		if !hasChanges {
+			return nil
+		}
+
+		stdout, err := tf.ShowPlanFileRaw(ctx, "tfplan", tfexec.Reattach(reattachInfo))
+
+		if err != nil {
+			return fmt.Errorf("error retrieving formatted plan output: %w", err)
+		}
+
+		t.Logf("plan: %s", stdout)
+
+		return nil
 	})
 
 	t.Log("Calling apply")
-	testRunProviderCommand(t, ctx, opts, wd, func() error {
-		return wd.Apply(ctx)
+	testRunProviderCommand(t, ctx, opts, tf, func(reattachInfo tfexec.ReattachInfo) error {
+		return tf.Apply(ctx, tfexec.Reattach(reattachInfo), tfexec.Refresh(false))
 	})
 
 	t.Log("Calling show")
-	testRunProviderCommand(t, ctx, opts, wd, func() error {
-		_, err := wd.State(ctx)
+	testRunProviderCommand(t, ctx, opts, tf, func(reattachInfo tfexec.ReattachInfo) error {
+		_, err := tf.Show(ctx, tfexec.Reattach(reattachInfo))
+
 		return err
 	})
 
 	t.Log("Calling refresh")
-	testRunProviderCommand(t, ctx, opts, wd, func() error {
-		return wd.Refresh(ctx)
+	testRunProviderCommand(t, ctx, opts, tf, func(reattachInfo tfexec.ReattachInfo) error {
+		return tf.Refresh(ctx, tfexec.Reattach(reattachInfo))
 	})
+	// testRunProviderCommand(t, ctx, opts, wd, func() error {
+	// 	return wd.Init(ctx)
+	// })
+
+	// t.Log("Calling plan")
+	// testRunProviderCommand(t, ctx, opts, wd, func() error {
+	// 	return wd.CreatePlan(ctx)
+	// })
+
+	// t.Log("Calling apply")
+	// testRunProviderCommand(t, ctx, opts, wd, func() error {
+	// 	return wd.Apply(ctx)
+	// })
+
+	// t.Log("Calling show")
+	// testRunProviderCommand(t, ctx, opts, wd, func() error {
+	// 	_, err := wd.State(ctx)
+	// 	return err
+	// })
+
+	// t.Log("Calling refresh")
+	// testRunProviderCommand(t, ctx, opts, wd, func() error {
+	// 	return wd.Refresh(ctx)
+	// })
 }
 
 func TestProtoV5ProviderFactoriesMerge(t *testing.T) {
